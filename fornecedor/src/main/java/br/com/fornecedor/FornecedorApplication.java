d40a70756aa067daf182875896cb30f3cb6f5aa3
alyson <alyson.nextage@gmail.com>
2022-12-01 18:34:15 -0300

    validação do token

--- fornecedor/src/main/java/br/com/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/com/fornecedor/FornecedorApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @Slf4j
 @EnableEurekaClient
+@EnableResourceServer
 public class FornecedorApplication {
 
     public static void main(String[] args) {
